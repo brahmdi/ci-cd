85a5ab5a4f617a054d3d85599b4a1afb1dc9d71b
brahmdi <hmdibrahim09@gmail.com>
2023-03-03 22:41:26 +0100

    first commit

---
+++ src/main/java/com/test/gitcicd/GitcicdApplication.java
@@ -0,0 +1,13 @@
+package com.test.gitcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitcicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitcicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/gitcicd/GitcicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.gitcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitcicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
