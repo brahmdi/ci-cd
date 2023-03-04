5c3a496f40695250e7460d8c4994227eaeb33366
brahmdi <hmdibrahim09@gmail.com>
2023-03-04 01:18:34 +0100

    add test junit

---
+++ src/main/java/com/test/gitcicd/Calcul.java
@@ -0,0 +1,13 @@
+package com.test.gitcicd;
+
+public class Calcul {
+    private int a ;
+    private int b ;
+    public Calcul(int a , int b){
+        this.a = a;
+        this.b = b ;
+    }
+    public int somme(){
+        return this.a + this.b ;
+    }
+}
---
+++ src/test/java/CalculTest.java
@@ -0,0 +1,19 @@
+import com.test.gitcicd.Calcul;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculTest {
+
+    Calcul calcul ;
+
+    @BeforeEach
+    void setup(){
+        calcul = new Calcul(1,2) ;
+    }
+    @Test
+    public  void testSomme(){
+        assertEquals(3,calcul.somme()) ;
+    }
+}
