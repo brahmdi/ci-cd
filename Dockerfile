696b38783c65fdcd8817914ff9f96b5785d88420
brahmdi <hmdibrahim09@gmail.com>
2023-03-22 22:37:48 +0100

    creat docker image

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
-FROM openjdk:8
-EXPOSE 8080
-ADD target/cicdjava-image.jar cicdjava-image.jar
-ENTRYPOINT ["java","-jar","/cicdjava-image.jar"]
+#Comme base de notre image, nous prendrons Alpine Linux compatible Java créé dans la section précédente.
+FROM openjdk:17-jdk-alpine
+#MAINTAINER baeldung.com
+MAINTAINER baeldung.com
+EXPOSE 8082
+ARG JAR_FILE=target/*.jar
+#Nous laissons Docker copier notre fichier jar dans l'image.
+COPY ./target/gitcicd.jar app.jar
+#Ce sera l'exécutable à démarrer au démarrage du conteneur.
+ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/test/gitcicd/GitcicdApplication.java
+++ src/main/java/com/test/gitcicd/GitcicdApplication.java
@@ -2,9 +2,17 @@ package com.test.gitcicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GitcicdApplication {
+
+    @GetMapping("/")
+    public String home(){
+        return "hello , this is your custom image !" ;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GitcicdApplication.class, args);
